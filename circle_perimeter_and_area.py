952dba315018c5acf150df71429790dfa35b640f
Dahrio-Francois <dahrio.francois@mths.ca>
2022-03-02 17:39:37 +0000

    made some changes to circle_perimeter_and_area.py

--- circle_perimeter_and_area.py
+++ circle_perimeter_and_area.py
@@ -3,7 +3,7 @@
 # Created by: Dahrio Francois
 # Created on: February 2022
 # This program calculates the perimeter and area of a circle
-#    with a radius of 15mm
+#    with a radius of 15 mm
 
 import math
 
@@ -11,10 +11,10 @@ import math
 def main():
     # this function calculates the area and perimeter
 
-    print("If a circle has the radius of 15mm: ")
+    print("If a circle has the radius of 15 mm: ")
     print("")
-    print("Area is {}mm².".format(math.pi * 15**2))
-    print("Perimeter is {}mm².".format(2 * math.pi * 15))
+    print("Area is {} mm².".format(math.pi * 15**2))
+    print("Perimeter is {} mm².".format(2 * math.pi * 15))
     print("")
     print("Done.")
 
